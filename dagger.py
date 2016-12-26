3fff51764036aeca9a8934117b4894bcd7b89e67
Avi Singh <avisingh599@gmail.com>
2016-12-26 11:46:29 -0800

    Add working dagger

--- dagger.py
+++ dagger.py
@@ -1,13 +1,23 @@
 from gym_torcs import TorcsEnv
 import numpy as np
 
+img_dim = [64,64,3]
+action_dim = 1
+steps = 1000
+batch_size = 32
+nb_epoch = 100
+
 def get_teacher_action(ob):
     steer = ob.angle*10/np.pi
     steer -= ob.trackPos*0.10
     return np.array([steer])
 
-img_dim = [64,64,3]
-action_dim = 1
+def img_reshape(input_img):
+    _img = np.transpose(input_img, (1, 2, 0))
+    _img = np.flipud(_img)
+    _img = np.reshape(_img, (1, img_dim[0], img_dim[1], img_dim[2]))
+    return _img
+
 images_all = np.zeros((0, img_dim[0], img_dim[1], img_dim[2]))
 actions_all = np.zeros((0,action_dim))
 rewards_all = np.zeros((0,))
@@ -15,7 +25,6 @@ rewards_all = np.zeros((0,))
 img_list = []
 action_list = []
 reward_list = []
-steps = 1000
 
 env = TorcsEnv(vision=True, throttle=False)
 ob = env.reset(relaunch=True)
@@ -38,20 +47,16 @@ env.end()
 
 print('Packing data into arrays...')
 for img, act, rew in zip(img_list, action_list, reward_list):
-    _img = np.transpose(img, (1, 2, 0))
-    _img = np.flipud(_img)
-    _img = np.reshape(_img, (1, 64, 64, 3))
-    images_all = np.concatenate([images_all, _img], axis=0)
+    images_all = np.concatenate([images_all, img_reshape(img)], axis=0)
     actions_all = np.concatenate([actions_all, np.reshape(act, [1,action_dim])], axis=0)
     rewards_all = np.concatenate([rewards_all, rew], axis=0)
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import Adam
 
-batch_size = 32
-nb_epoch = 100
-
+#model from https://github.com/fchollet/keras/blob/master/examples/cifar10_cnn.py
 model = Sequential()
 
 model.add(Convolution2D(32, 3, 3, border_mode='same',
@@ -77,29 +82,46 @@ model.add(Dense(action_dim))
 model.add(Activation('tanh'))
 
 model.compile(loss='mean_squared_error',
-              optimizer='adam',
+              optimizer=Adam(lr=1e-4),
               metrics=['mean_squared_error'])
 
 model.fit(images_all, actions_all,
               batch_size=batch_size,
               nb_epoch=nb_epoch,
-              #validation_data=(X_test, Y_test),
               shuffle=True)
 
-#evaluate
+output_file = open('results.txt', 'w')
 
-env = TorcsEnv(vision=True, throttle=False)
-ob = env.reset(relaunch=True)
-reward_sum = 0.0
-for i in range(steps):
-    _img = np.transpose(ob.img, (1, 2, 0))
-    _img = np.flipud(_img)
-    _img = np.reshape(_img, (1, 64, 64, 3))
-    act = model.predict(_img)
-    ob, reward, done, _ = env.step(act)
-    if done is True:
+#aggregate and retrain
+dagger_itr = 5
+for itr in range(dagger_itr):
+    ob_list = []
+
+    env = TorcsEnv(vision=True, throttle=False)
+    ob = env.reset(relaunch=True)
+    reward_sum = 0.0
+
+    for i in range(steps):
+        act = model.predict(img_reshape(ob.img))
+        ob, reward, done, _ = env.step(act)
+        if done is True:
+            break
+        else:
+            ob_list.append(ob)
+        reward_sum += reward
+        print(i, reward, reward_sum, done, str(act[0]))
+    print('Episode done ', itr, i, reward_sum)
+    output_file.write('Number of Steps: %02d\t Reward: %0.04f\n'%(i, reward_sum))
+    env.end()
+
+    if i==(steps-1):
         break
-    reward_sum += reward
-    print(i, reward, reward_sum, done, str(act[0]))
 
-env.end()
+    for ob in ob_list:
+        images_all = np.concatenate([images_all, img_reshape(ob.img)], axis=0)
+        actions_all = np.concatenate([actions_all, np.reshape(get_teacher_action(ob), [1,action_dim])], axis=0)
+
+    model.fit(images_all, actions_all,
+                  batch_size=batch_size,
+                  nb_epoch=nb_epoch,
+                  shuffle=True)
